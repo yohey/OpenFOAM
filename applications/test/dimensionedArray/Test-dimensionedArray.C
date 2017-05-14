ae53ad05b7441cbdcab85115e9917e8876ff7958
Yohey Ishizu <yohey.ishizu@gmail.com>
2017-05-14 16:05:48 +0900

    dimensionedArray2 を追加

--- applications/test/dimensionedArray/Test-dimensionedArray.C
+++ applications/test/dimensionedArray/Test-dimensionedArray.C
@@ -7,15 +7,23 @@ using namespace Foam;
 int main(int argc, char *argv[])
 {
     dimensionedArray D("D", dimLength, array::zeros(5));
+    dimensionedArray2 SigmaS("SigmaS", dimless/dimLength, array2::zeros(5, 5));
 
     Info << D << endl;
+    Info << SigmaS << endl;
 
 
     dictionary dict(IFstream("testDict")());
-    ITstream its("D", dict.lookup("D"));
-    its >> D;
 
+    ITstream *its;
+
+    its = new ITstream("D", dict.lookup("D"));
+    *its >> D;
     Info << D << endl;
+
+    its = new ITstream("SigmaS", dict.lookup("SigmaS"));
+    *its >> SigmaS;
+    Info << SigmaS << endl;
 
     return 0;
 }
